e02aa0daa70f1e07efbea021e3027590bc6c48ab
debezet <debezet@gmail.com>
2012-03-05 22:06:08 +0100

    Layout and missing buttons, rawr
    It's good because fuck you and works

--- dp/Foo.java
+++ dp/Foo.java
@@ -16,21 +16,20 @@ public class Foo extends JPanel
 	BufferedImage buffer;
 	int x1, x2, y1, y2;
 
-	//TODO Ÿle. Nie rysuje, pewnie jakiœ g³upi b³¹d - popraw plz 0.o
-	//super
+	//Machade³ko
 	@Override
 	    public void paintComponent(Graphics g) 
 	 {
 		 Graphics2D g2d = (Graphics2D) g;       
-	        g2d.setColor(Color.black);
+	        g2d.setColor(Color.white);
 	        g2d.fillRect(0, 0, 800, 600);
-	        g2d.setPaint(Color.red);
+	        g2d.setPaint(Color.black);
 	        g2d.drawLine(330, 220, x1, y1);
 	        g2d.drawLine(x1, y1, x2, y2);
 	        g2d.setPaint(Color.GREEN);
-	        g2d.fillOval(x1-7, y1-7, 15, 15);
+	        g2d.fillOval(x1-10, y1-10, 20, 20);
 	        g2d.setPaint(Color.YELLOW);
-	        g2d.fillOval(x2-7, y2-7, 15, 15);
+	        g2d.fillOval(x2-10, y2-10, 20, 20);
 	    }
 	public void setCoord(int x1, int y1, int x2, int y2)
 	{
